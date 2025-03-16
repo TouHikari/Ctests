e24631677a9417440fa5957dadd1bbbf2a464fab
TouHikari <tang20051026@foxmail.com>
2025-03-16 22:25:50 +0800

    compute pi

--- Data Structure Homework/experiment/Chapter 2/2.c
+++ Data Structure Homework/experiment/Chapter 2/2.c
@@ -1,12 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 双向链表
+// 定义双向链表
 typedef struct DNode {
     int data;               // 数据域
     struct DNode * prior;   // 前驱指针域
     struct DNode * next;    // 后继指针域
 } DNode, * DLinkList;
+
+// 初始化一个双向链表
+DLinkList InitDLinkList(DLinkList *DL);
+// 销毁双向链表
+void DestroyDLinkList(DLinkList *DL);
+// 向链表末尾添加元素
+void DLinkInsertEnd(DLinkList DL, int data);
+// 遍历链表并输出指定位数的小数
+// 链表第一个结点中存储整数部分，之后每个结点存储一位小数
+void PrintDLinkList(DLinkList DL, int digits);
+// 大数加法，将结果保存在 num2 中，两个加数必须小数位相同
+DLinkList BigAdd(DLinkList num1, DLinkList num2);
+// 大数与一个 int 型的乘法，原地相乘将结果保存在 num 中
+DLinkList BigMul(DLinkList num, int multiplier);
+// 大数与一个 int 型的除法，原地相除将结果保存在 num 中
+DLinkList BigDiv(DLinkList num, int divisor);
+// 精确计算 Pi 的值，至少保证小数点后 digits 位的正确性
+DLinkList ComputePi(int digits);
+
+int main(void)
+{
+    // 存放结果的双向链表
+    DLinkList result;
+    
+    // 读取要求输出的的 Pi 的小数位数
+    int n;
+    scanf("%d", &n);
+    
+    // 将计算结果写入链表
+    result = ComputePi(n);
+    
+    // 打印计算结果
+    PrintDLinkList(result, n);
+    
+    // 释放内存
+    DestroyDLinkList(&result);
+    
+    return 0;
+}
 
 // 初始化一个双向链表
 DLinkList InitDLinkList(DLinkList *DL)
@@ -25,12 +64,26 @@ DLinkList InitDLinkList(DLinkList *DL)
         return NULL;
 }
 
+// 销毁双向链表
+void DestroyDLinkList(DLinkList *DL)
+{
+    DNode * p = (*DL)->next;
+    while (p != NULL)   // 从表头的下一个结点开始依次释放各结点内存
+    {
+        DNode * temp = p;
+        p = p->next;
+        free(temp);
+    }
+    free(*DL);          // 最后释放表头内存
+    *DL = NULL;         // 防止悬空指针
+}
+
 // 向链表末尾添加元素
 void DLinkInsertEnd(DLinkList DL, int data)
 {
-    DLinkList p = DL;       // 声明链表指针
+    DLinkList p = DL;   // 声明链表指针
     while (p->next)
-        p = p->next;        // 找到最后一个结点
+        p = p->next;    // 找到最后一个结点
 
     // 建立新结点，并且连接到链表中
     DLinkList p_new = (DLinkList)malloc(sizeof(DNode));
@@ -55,25 +108,87 @@ void PrintDLinkList(DLinkList DL, int digits)
     {
         printf("%d", p->data);
         if (p->next)        // 若未到链表末尾
-            p = p->next;    // 指针后移
+            p = p->next;    // 指针后移一位
     }
     printf("\n");           // 打印完成后换行
 }
 
-// 精确计算 Pi 的值，至少保证小数点后 500 位的正确性
-DLinkList ComputePi(void)
+// 大数加法，将结果保存在 num2 中，两个加数必须小数位相同
+DLinkList BigAdd(DLinkList num1, DLinkList num2)
+{
+    // 创建两个指针，分别移动到两个链表末尾
+    DLinkList p1 = num1;
+    DLinkList p2 = num2;
+    while (p1->next)
+        p1 = p1->next;
+    while (p2->next)
+        p2 = p2->next;
+
+    int ret = 0;        // 存放进位值
+    int temp = 0;       // 存放临时值
+    while (p1 && p2)    // 相加并将结果保存在 num2 中
+    {
+        temp = p1->data + p2->data + ret;
+        ret = temp / 10;
+        p2->data = temp % 10;
+        p1 = p1->prior;
+        p2 = p2->prior;
+    }
+    return num2;
+}
+
+// 大数与一个 int 型的乘法，原地相乘将结果保存在 num 中
+DLinkList BigMul(DLinkList num, int multiplier)
+{
+    // 创建指针，移动到链表末尾
+    DLinkList p = num;
+    while (p->next)
+        p = p->next;
+    
+    int ret = 0;        // 存放进位值
+    int temp = 0;       // 存放临时值
+    while (p)
+    {
+        temp = p->data * multiplier + ret;
+        p->data = temp % 10;
+        ret = temp / 10;
+        p = p->prior;
+    }
+    return num;
+}
+
+// 大数与一个 int 型的除法，原地相除将结果保存在 num 中
+DLinkList BigDiv(DLinkList num, int divisor)
+{
+    DLinkList p = num->next;
+
+    int ret = 0;        // 存放进位值
+    int temp = 0;       // 存放临时值
+    while (p)
+    {
+        temp = p->data + ret * 10;
+        ret = temp % divisor;
+        p->data = temp / divisor;
+        p = p->next;
+    }
+    return num;
+}
+
+// 精确计算 Pi 的值，至少保证小数点后 digits 位的正确性
+DLinkList ComputePi(int digits)
 {
     DLinkList num;          // num 为每次相加的 R(n)
     DLinkList sum;          // sum 最终的值约等于 Pi
     InitDLinkList(&num);
     InitDLinkList(&sum);
 
-    // 给链表补够 600 位来进行计算，避免计算精度影响到最后几位结果
-    for (int i = 0; i < 600; i++)
+    // 给链表多补 100 位来进行计算，避免计算精度影响到最后几位结果
+    for (int i = 0; i < digits + 100; i++)
     {
         DLinkInsertEnd(num, 0);
         DLinkInsertEnd(sum, 0);
     }
+
     // p1, p2 为指向两个链表的指针 
     DLinkList p1 = num->next;
     DLinkList p2 = sum->next;
@@ -81,68 +196,14 @@ DLinkList ComputePi(void)
     p1->data = 2;
     p2->data = 2;
 
-    // 指向 num 和 sum 的尾节点
-    while (p1->next)
-        p1 = p1->next;
-    DLinkList numtail = p1;
-    while (p2->next)
-        p2 = p2->next;
-    DLinkList sumtail = p2;
-
     // 开始运算
-    int temp = 0;   // 计算过程中产生的临时数据
-    int ret = 0;    // 计算过程中产生的借位 or 进位
-    int t;          // 计算时有规律出现的除数 2n + 1
-    for (int j = 1, t = 3; j < 10000; j++, t = t + 2)
+    for (int j = 1, t = 3; j < 10000; j++, t += 2)
     {
-        // j 同时承担计数和做乘数的任务
-        // 先计算乘法（即公式中 R(n) * n）
-        DLinkList p3 = numtail;
-        ret = 0;
-        while (p3)
-        {
-            temp = p3->data * j + ret;
-            p3->data = temp % 10;
-            ret = temp / 10;
-            p3 = p3->prior;
-        }
-        ret = 0;
-        p3 = num->next;
-        // 计算除法（即公式中的 R(n) * n / (2 * n + 1)）
-        while (p3)
-        {
-            temp = p3->data + ret * 10;
-            ret = temp % t;
-            p3->data = temp / t;
-            p3 = p3->next;
-        }
-        ret = 0;
-        DLinkList  p4 = sumtail;
-        p3 = numtail;
-        
-        // 将得到的 R(n) 的值加入到结果中
-        while (p3 && p4)
-        {
-            temp = p3->data + p4->data + ret;
-            ret = temp / 10;
-            p4->data = temp % 10;
-            p3 = p3->prior;
-            p4 = p4->prior;
-        }
+        BigMul(num, j);
+        BigDiv(num, t);
+        BigAdd(num, sum);
     }
+
+    DestroyDLinkList(&num);
     return sum;
-}
-
-int main(void)
-{
-    DLinkList result;
-
-    // 读取要求输出的的 Pi 的小数位数
-    int n;
-    scanf("%d", &n);
-
-    result = ComputePi();
-
-    PrintDLinkList(result, n);
-    return 0;
 }
